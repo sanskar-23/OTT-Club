1ef236219738fd15891a9be63d369be80611794f
Sanskar Kapoor <76424866+sanskar-23@users.noreply.github.com>
2025-07-12 18:52:47 +0530

    Done with Setup of Redux store in our application

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 import "./index.scss";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
