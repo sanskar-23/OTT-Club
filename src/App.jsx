ac4d6cd92e59b4dc2301b7a3cdade06948bfa5df
Sanskar Kapoor <76424866+sanskar-23@users.noreply.github.com>
2025-07-12 20:25:31 +0530

    Created react-router dom with all the routes associated to each component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
 import { getApiConfiguration } from "./store/homeSlice";
 import { useSelector, useDispatch } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/pageNotFound";
 
 function App() {
   const dispatch = useDispatch();
@@ -18,7 +26,19 @@ function App() {
     });
   };
 
-  return <div>Total Pages in this application are : {url?.total_pages}</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
