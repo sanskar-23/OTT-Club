718cb242ed5237317acec1e84519c569263500fb
Sanskar Kapoor <76424866+sanskar-23@users.noreply.github.com>
2025-07-12 19:51:21 +0530

    Created the placeholder jsx components for all the other components used in our application

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
+import { getApiConfiguration } from "./store/homeSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+
   useEffect(() => {
     apiTesting();
   }, []);
@@ -9,10 +14,11 @@ function App() {
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
+      dispatch(getApiConfiguration(res));
     });
   };
 
-  return <div>App</div>;
+  return <div>Total Pages in this application are : {url?.total_pages}</div>;
 }
 
 export default App;
