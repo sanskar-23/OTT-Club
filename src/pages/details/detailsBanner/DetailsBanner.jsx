86e3b10b0076cc3f60b29b04e3d2eec8c5065234
Sanskar Kapoor <76424866+sanskar-23@users.noreply.github.com>
2025-07-26 20:21:20 +0530

    Added Play Button for trailers of TV Shows and Movies

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -12,8 +12,12 @@ import CircleRating from "../../../components/circleRating/CircleRating";
 import Img from "../../../components/lazyLoadImage/Img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
 import { PlayIcon } from "../Playbtn";
+import VideoPopup from "../../../components/videoPopup/VideoPopUp";
 
 const DetailsBanner = ({ video, crew }) => {
+  const [show, setShow] = useState(false);
+  const [videoId, setVideoId] = useState(null);
+
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}`);
   const { url } = useSelector((state) => state.home);
@@ -67,7 +71,10 @@ const DetailsBanner = ({ video, crew }) => {
                       <CircleRating rating={data?.vote_average.toFixed(1)} />
                       <div
                         className="playbtn"
-                        onClick={() => console.log("Clicked the play btn")}
+                        onClick={() => {
+                          setShow(true);
+                          setVideoId(video.key);
+                        }}
                       >
                         <PlayIcon />
                         <span className="text">Watch Trailer...</span>
@@ -140,6 +147,12 @@ const DetailsBanner = ({ video, crew }) => {
                     )}
                   </div>
                 </div>
+                <VideoPopup
+                  show={show}
+                  setShow={setShow}
+                  videoId={videoId}
+                  setVideoId={setVideoId}
+                />
               </ContentWrapper>
             </React.Fragment>
           )}
