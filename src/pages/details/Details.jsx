60496b94d931884b4183f96d9c7dc338428bf9f8
Sanskar Kapoor <76424866+sanskar-23@users.noreply.github.com>
2025-07-26 18:34:47 +0530

    Created DetailsBanner page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./style.scss";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
 
 const Details = () => {
-  return <div>Details</div>;
+  //const { mediaType, id } = useParams();
+  //const { data, loading } = useFetch(`/${mediaType}/${id}`);
+
+  return (
+    <div>
+      <DetailsBanner />
+    </div>
+  );
 };
 
 export default Details;
